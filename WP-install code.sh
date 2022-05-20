cb7c5ce83be863403d22ed3e9cb707cf0da8c9b7
EC2 Default User <ec2-user@ip-172-31-6-173.eu-west-1.compute.internal>
2022-05-20 11:39:49 +0000

    1

---
+++ WP-install code.sh
@@ -0,0 +1,41 @@
+sudo yum update -y
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+cat /etc/system-release
+sudo yum install -y httpd mariadb-server
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl is-enabled httpd
+sudo usermod -a -G apache ec2-user
+groups
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+rm /var/www/html/phpinfo.php
+sudo systemctl start mariadb
+sudo mysql_secure_installation
+sudo systemctl enable mariadb
+sudo yum install php-mbstring php-xml -y
+sudo systemctl restart httpd
+sudo systemctl restart php-fpm
+cd /var/www/html
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz
+mkdir phpMyAdmin && tar -xvzf phpMyAdmin-latest-all-languages.tar.gz -C phpMyAdmin --strip-components 1
+rm phpMyAdmin-latest-all-languages.tar.gz
+sudo systemctl start mariadb
+cd
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+ls
+mysql -u root -p
+cp wordpress/wp-config-sample.php wordpress/wp-config.php
+nano wordpress/wp-config.php
+cat wordpress/wp-config.php
+cp -r wordpress/* /var/www/html/
+sudo yum install php-gd
+sudo chown -R apache /var/www
+sudo chgrp -R apache /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0644 {} \;
+sudo systemctl restart httpd
