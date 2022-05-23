554fe7602b6b487d9b8dca4e853eb0079992607e
EC2 Default User <ec2-user@ip-172-31-6-173.eu-west-1.compute.internal>
2022-05-23 18:08:14 +0000

    ser ut att funka

--- WP-install code.sh
+++ WP-install code.sh
@@ -37,5 +37,5 @@ sudo chown -R apache /var/www
 sudo chgrp -R apache /var/www
 sudo chmod 2775 /var/www
 find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0644 {} \;
-sudo systemctl restart httpd
+41  find /var/www -type f -exec sudo chmod 0644 {} \;
+42  sudo systemctl restart httpd
